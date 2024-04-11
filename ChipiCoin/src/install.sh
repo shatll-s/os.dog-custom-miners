aa872a050a7a8742ce0b5022925f3cb1075daafb
shatll-s <shatll_s@mail.ru>
2024-04-11 15:23:40 +0300

    ChipiCoin: pow-miner-cuda is now in miner archive, new mount point is no longer created in tmpfs, instead a link to the directory in /tmp is used.

--- ChipiCoin/src/install.sh
+++ ChipiCoin/src/install.sh
@@ -38,15 +38,16 @@ if [[ $(NeedToInstall git) -eq 1 ]]; then
 	echo "> Install git"
 	apt install -yqq git
 else
-	echo "${GREEN}> git already installed${WHITE}"
+	echo -e "${GREEN}> git already installed${WHITE}"
 fi
 
 dir=files
 if [[ ! -d $dir/.git ]]; then
 	echo "> git dir does not exist, cloning"
 	git clone $GIT_REPO.git $dir
-	wget https://github.com/tontechio/pow-miner-gpu/releases/download/20211230.1/minertools-cuda-ubuntu-18.04-x86-64.tar.gz -O minertools.tar.gz
-	tar -xzvf minertools.tar.gz -C $dir
+	# now there is no need to do it, files is already in package
+	#wget https://github.com/tontechio/pow-miner-gpu/releases/download/20211230.1/minertools-cuda-ubuntu-18.04-x86-64.tar.gz -O minertools.tar.gz
+	#tar -xzvf minertools.tar.gz -C $dir
 	cd $dir
 	npm i
 else
@@ -59,7 +60,7 @@ fi
 cd ..
 
 # if we have modified files, than change them
-filesToChange=("send_multigpu.js" "givers.js")
+filesToChange=("send_multigpu.js" "givers.js" "pow-miner-cuda")
 for (( i = 0; i < ${#filesToChange[@]}; i++ )); do
     fileToChange=${filesToChange[$i]}
     if [[ ! -f $fileToChange ]]; then
@@ -67,11 +68,14 @@ for (( i = 0; i < ${#filesToChange[@]}; i++ )); do
       continue
     fi
 
-    echo -e "${GREEN}> Replace ${CYAN}$fileToChange${WHITE}${WHITE}"
-    cp $fileToChange $dir/$
+    echo -e "${GREEN}> Replace ${CYAN}$fileToChange${WHITE}"
+    cp $fileToChange $dir/$fileToChange
 done
 
-[[ ! `cat /etc/mtab | grep "$dir/bocs tmpfs"` ]] && mount -t tmpfs tmpfs $dir/bocs -o size=20m
+tmpDirName=/tmp/bocs`date +%s`
+mkdir $tmpDirName
+rm -rf `realpath $dir`/bocs
+ln -s $tmpDirName `realpath $dir`/bocs
 
 echo -e "${GREEN}> install script complete${WHITE}"
 
