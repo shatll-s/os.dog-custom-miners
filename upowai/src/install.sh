5c394b78570461174c305c632c6c2b62b265ef28
shatll <shatll_s@mail.ru>
2024-05-16 11:15:15 +0300

    upowai: install script fix

--- upowai/src/install.sh
+++ upowai/src/install.sh
@@ -13,7 +13,7 @@ if [[ $(NeedToInstall libjson-c5) -eq 1 ]]; then
   sed '/deb http:\/\/cz\.archive\.ubuntu\.com\/ubuntu jammy main/d' -i /etc/apt/sources.list
 	echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list
 	apt update
-	apt install libc6 -yqq
+	apt install libjson-c5 -yqq
 else
 	echo -e "${GREEN}> libc6 already installed${WHITE}"
 fi
