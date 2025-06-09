26a8cab0698943582f99f9ddb627c3f15a664bea
shatll <shatll_s@mail.ru>
2025-06-09 20:31:44 +0300

    qubitcoin-miner: 1.01

--- qubitcoin-miner/src/install.sh
+++ qubitcoin-miner/src/install.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 set -e
 
+# Проверка: уже установлено?
 if ldconfig -p | grep -q libcublas.so.12; then
   echo "[✔] Уже установлено: libcublas.so.12"
   exit 0
 fi
 
-echo "[*] Проверка версии Ubuntu"
+# Определение версии Ubuntu и нужных переменных
 UBU_VERSION=$(lsb_release -sr)
-if [[ "$UBU_VERSION" != "22.04" ]]; then
-  echo "[!] Скрипт предназначен для Ubuntu 22.04, а не $UBU_VERSION"
-  exit 1
-fi
+case "$UBU_VERSION" in
+  "22.04")
+    CUDA_REPO="ubuntu2204"
+    ;;
+  "20.04")
+    CUDA_REPO="ubuntu2004"
+    ;;
+  *)
+    echo "[!] Скрипт поддерживает только Ubuntu 20.04 и 22.04, а не $UBU_VERSION"
+    exit 1
+    ;;
+esac
 
 echo "[*] Установка зависимостей"
 apt update
 apt install -y wget ca-certificates gnupg lsb-release
 
 echo "[*] Скачивание pin-файла"
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/repos/${CUDA_REPO}/x86_64/cuda-${CUDA_REPO}.pin
+mv cuda-${CUDA_REPO}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
 echo "[*] Установка GPG-ключа репозитория"
-curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub \
-  | gpg --dearmor --yes | tee /usr/share/keyrings/cuda-archive-keyring.gpg > /dev/null
+curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/${CUDA_REPO}/x86_64/3bf863cc.pub \
+  | gpg --dearmor | tee /usr/share/keyrings/cuda-archive-keyring.gpg > /dev/null
 
 echo "[*] Добавление репозитория CUDA"
-echo "deb [signed-by=/usr/share/keyrings/cuda-archive-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" \
+echo "deb [signed-by=/usr/share/keyrings/cuda-archive-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/${CUDA_REPO}/x86_64/ /" \
   > /etc/apt/sources.list.d/cuda.list
 
 echo "[*] Обновление apt"
 apt update
 
-echo "[*] Установка библиотек CUDA (включая libcublas.so.12)"
-#apt install -y cuda-libraries-12-4
-apt install -y libcublas-12-4 cuda-libraries-12-4
+echo "[*] Установка библиотек CUDA (libcublas.so.12, libcudart.so.12 и др)"
+apt install -y cuda-libraries-12-4
 
 echo "[*] Проверка установки libcublas.so.12"
 if ldconfig -p | grep -q libcublas.so.12; then
