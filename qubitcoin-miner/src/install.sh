37ead5db938ba2cc38aebae704d8cd5ff8a4819a
shatll <shatll_s@mail.ru>
2025-06-09 21:07:14 +0300

    qubitcoin-miner: 1.03

--- qubitcoin-miner/src/install.sh
+++ qubitcoin-miner/src/install.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 set -e
 
+[ -t 1 ] && . /dog/colors
+
+function NeedToInstall() {
+	local ver=`apt-cache policy $1 | grep Installed | sed 's/Installed://; s/\s*//'`
+	if [[ $2 ]]; then #min ver provided
+    local majorVer=$(echo $ver | cut -d- -f1)
+    if (( $(echo "$majorVer > $2" | bc -l) )); then
+        echo 0
+    else
+        echo 1
+    fi
+  else
+	  [[ $ver && $ver != '(none)' ]] && echo 0 || echo 1
+  fi
+}
+
+if [[ $(NeedToInstall libc6 "2.32") -eq 1 ]]; then
+	echo -e "> Install libc6"
+	echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list
+	apt update
+	apt install libc6 -yqq
+else
+	echo -e "${GREEN}> libc6 already installed${WHITE}"
+fi
+
 # Проверка: уже установлено?
 if ldconfig -p | grep -q libcublas.so.12; then
   echo "[✔] Уже установлено: libcublas.so.12"
