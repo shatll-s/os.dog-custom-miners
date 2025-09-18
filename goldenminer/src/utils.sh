2ba2ffc999d2177d9350bed2e85eeddd19aded6d
shatll-s <shatll_s@mail.ru>
2025-09-18 19:29:43 +0300

    goldenminer: 0.4 (devices concept)

---
+++ goldenminer/src/utils.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# parse args
+parse_args() {
+    local args="$1"
+    shift
+    local keys=("$@")
+
+    read -ra tokens <<< "$args"
+    local filtered=()
+
+    for ((i = 0; i < ${#tokens[@]}; i++)); do
+        local token="${tokens[i]}"
+        local is_key=0
+
+        for key in "${keys[@]}"; do
+            if [[ "$token" == "--$key" ]]; then
+                local var_name="${key//-/_}"
+                if (( i + 1 < ${#tokens[@]} )); then
+                    local value="${tokens[$((i+1))]}"
+                    export "${var_name}=$value"
+                else
+                    export "${var_name}="
+                fi
+                ((i++)) # skip the value
+                is_key=1
+                break
+            fi
+        done
+
+        if [[ $is_key -eq 0 ]]; then
+            filtered+=("${token}")
+        fi
+    done
+
+    REMAINING_ARGS="${filtered[*]}"  # Set global variable instead
+}
